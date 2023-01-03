7485fe1356fc758adedc443d0cdc0a419b658ecd
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2023-01-03 11:53:55 +0100

    etherjs added

--- counter/index.js
+++ counter/index.js
@@ -1,7 +1,26 @@
-require('dotenv').config();
-const ethers = require('ethers');
+require("dotenv").config();
+const ethers = require("ethers");
+const contractABI = require('./ABI.json');
 
-function main() {
-    // new ethers.Contract( address , abi , signerOrProvider )
-    const counterContract = new ethers.Contract('0x5F91eCd82b662D645b15Fd7D2e20E5e5701CCB7A' , abi , signerOrProvider );
+
+//new ethers.providers.AlchemyProvider( [ network = "homestead" , [ apiKey ] ] )
+const provider = new ethers.providers.AlchemyProvider(
+    "goerli",
+    process.env.TEST_API_KEY
+);
+
+async function main() {
+    console.log ("Gettin the current count:");
+  // new ethers.Contract( address , abi , signerOrProvider )
+    const counterContract = new ethers.Contract(
+        "0x5F91eCd82b662D645b15Fd7D2e20E5e5701CCB7A",
+        contractABI,
+        provider
+    );
+
+    const currentCounterValue = await counterContract.count();
+
+    console.log (currentCounterValue.toString());
 }
+
+main();
